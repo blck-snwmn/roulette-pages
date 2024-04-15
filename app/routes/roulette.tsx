e729866ac91a80cf722006f8f1b5bd988ab021d2
blck-snwmn <whitesnowmancreator@gmail.com>
2024-04-16 01:30:36 +0900

    Increment index

--- app/routes/roulette.tsx
+++ app/routes/roulette.tsx
@@ -227,14 +227,19 @@ const Roulette = () => {
 	const [isSpinning, setIsSpinning] = useState<boolean>(false);
 	const [rotation, setRotation] = useState<number>(0);
 	const [history, setHistory] = useState<HistoryItem[]>([]);
+	const [nextIndex, setNextIndex] = useState(0);
 
 	const handleAnimationEnd = (newRotation: number, resultIndex: number) => {
 		setRotation(newRotation);
 		const newSelectedItem = items[resultIndex];
-		setHistory((prev) => [
-			{ index: prev.length, item: newSelectedItem },
-			...prev.slice(0, MAX_HISTORY_LENGTH - 1),
-		]);
+		setHistory((prev) => {
+			const newHistory = [
+				{ index: nextIndex, item: newSelectedItem },
+				...prev.slice(0, MAX_HISTORY_LENGTH - 1),
+			];
+			setNextIndex(nextIndex + 1);
+			return newHistory;
+		});
 		setIsSpinning(false);
 	};
 
