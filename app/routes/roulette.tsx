a8c35eafe35cbf69f4bfe3410081613ca9058ea1
blck-snwmn <whitesnowmancreator@gmail.com>
2024-04-16 01:19:04 +0900

    Show history position

--- app/routes/roulette.tsx
+++ app/routes/roulette.tsx
@@ -179,35 +179,39 @@ interface HistoryProps {
 	history: HistoryItem[];
 }
 
+const historyHeight = 600; // 履歴の表示領域の高さ(px)
+
 const History = ({ history }: HistoryProps) => {
 	const current = history.length > 0 ? history[0] : null;
 
 	return (
-		<div className="md:w-1/3 overflow-auto p-4">
-			<h2 className="text-lg font-bold mb-2">履歴</h2>
-			{current && (
-				<div
-					className="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4"
-					role="alert"
-				>
-					<p className="font-bold">最新の結果</p>
-					<p>{current.item}</p>
-				</div>
-			)}
-			<ul>
-				{history.map(({ index, item }) => (
-					<li
-						key={index}
-						className={`border-b border-gray-200 py-2 ${
-							index === current?.index && item === current?.item
-								? "font-bold"
-								: ""
-						}`}
+		<div className="md:w-1/3 flex flex-col justify-center p-4">
+			<div className="overflow-auto" style={{ height: `${historyHeight}px` }}>
+				<h2 className="text-lg font-bold mb-2">履歴</h2>
+				{current && (
+					<div
+						className="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4"
+						role="alert"
 					>
-						{index + 1}. {item}
-					</li>
-				))}
-			</ul>
+						<p className="font-bold">最新の結果</p>
+						<p>{current.item}</p>
+					</div>
+				)}
+				<ul>
+					{history.map(({ index, item }) => (
+						<li
+							key={index}
+							className={`border-b border-gray-200 py-2 ${
+								index === current?.index && item === current?.item
+									? "font-bold"
+									: ""
+							}`}
+						>
+							{index + 1}. {item}
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
