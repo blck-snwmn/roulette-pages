42c92cdbd43e939e8f82e31ef813e27871992998
blck-snwmn <whitesnowmancreator@gmail.com>
2024-05-02 23:39:48 +0900

    Add colors util

--- app/components/RouletteCanvas.tsx
+++ app/components/RouletteCanvas.tsx
@@ -1,27 +1,8 @@
 import { useEffect, useRef, useState } from "react";
-
-const colorSet = [
-    { h: 0, s: 50, l: 80 }, // 赤
-    { h: 30, s: 50, l: 80 }, // オレンジ
-    { h: 60, s: 50, l: 80 }, // 黄色
-    { h: 120, s: 50, l: 80 }, // 緑
-    { h: 180, s: 50, l: 80 }, // 青
-    { h: 240, s: 50, l: 80 }, // 紫
-    { h: 270, s: 50, l: 80 }, // マゼンタ
-    { h: 300, s: 50, l: 80 }, // ピンク
-    { h: 330, s: 50, l: 80 }, // 赤紫
-    { h: 210, s: 50, l: 80 }, // 水色
-];
-
-const generateColors = (numColors: number) => {
-    const colors = [];
-    for (let i = 0; i < numColors; i++) {
-        colors.push(colorSet[i % colorSet.length]);
-    }
-    return colors;
-};
+import { generateColors } from "~/utils/colorUtils";
 
 type Colors = ReturnType<typeof generateColors>;
+
 interface RouletteCanvasProps {
     items: string[];
     rotation: number;
